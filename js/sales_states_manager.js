5c5253595733b73a8fb57dc7e98f7dd3cc8074c0
Jose F Rivas C <josefelixrc7@gmail.com>
2025-02-02 12:44:27 -0400

    feat: Improve estadoDeVentas

--- js/sales_states_manager.js
+++ js/sales_states_manager.js
@@ -1,30 +1,45 @@
 $(function()
 {
-    function FilterSales(agent_id, sales)
+    function FilterSales(agent_id, from, to, sales)
     {
-        return sales.filter(sale => sale.agent_id == agent_id);
+        return sales.filter(sale => sale.agent_id == agent_id && sale.columns[2] >= from && sale.columns[2] <= to);
     }
 
     var sales = GetSales();
 
-    $('#search').click(function()
+    $('#search').submit(function(e)
     {
+        e.preventDefault();
         var agent_id = $('#agent_id').val();
+        var from = $('#from').val();
+        var to = $('#to').val();
 
-        if (agent_id === "")
+        if (agent_id == "")
         {
             $('.notifications').empty();
             $('.notifications').append('<div class="alert alert-warning" role="alert">Por favor, ingrese su identificación</div>');
             return;
         }
-
-        var filtered_sales = FilterSales(agent_id, sales);
-        $('#results_table tbody').empty();
-
-        if (filtered_sales.length === 0)
+        if (from == "")
         {
             $('.notifications').empty();
-            $('.notifications').append('<div class="alert alert-info" role="alert">No se encontraron ventas para el agente con identificación ' + agent_id + '</div>');
+            $('.notifications').append('<div class="alert alert-warning" role="alert">Por favor, ingrese fecha "Desde"</div>');
+            return;
+        }
+        if (to == "")
+        {
+            $('.notifications').empty();
+            $('.notifications').append('<div class="alert alert-warning" role="alert">Por favor, ingrese fecha "Hasta"</div>');
+            return;
+        }
+
+        var filtered_sales = FilterSales(agent_id, from, to, sales);
+        $('#results_table tbody').empty();
+
+        if (filtered_sales.length == 0)
+        {
+            $('.notifications').empty();
+            $('.notifications').append('<div class="alert alert-info" role="alert">No se encontraron ventas para el agente con identificación ' + agent_id + ' en el rango de tiempo establecido</div>');
             return;
         }
         $('.notifications').empty();
