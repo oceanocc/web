26fd046983bd30e73ac4ee2041cff9f66eb83d63
Jose F Rivas C <josefelixrc7@gmail.com>
2025-02-02 12:24:42 -0400

    feat: Add Estado De Ventas functions and page

---
+++ js/sales_states_manager.js
@@ -0,0 +1,41 @@
+$(function()
+{
+    function FilterSales(agent_id, sales)
+    {
+        return sales.filter(sale => sale.agent_id == agent_id);
+    }
+
+    var sales = GetSales();
+
+    $('#search').click(function()
+    {
+        var agent_id = $('#agent_id').val();
+
+        if (agent_id === "")
+        {
+            $('.notifications').empty();
+            $('.notifications').append('<div class="alert alert-warning" role="alert">Por favor, ingrese su identificación</div>');
+            return;
+        }
+
+        var filtered_sales = FilterSales(agent_id, sales);
+        $('#results_table tbody').empty();
+
+        if (filtered_sales.length === 0)
+        {
+            $('.notifications').empty();
+            $('.notifications').append('<div class="alert alert-info" role="alert">No se encontraron ventas para el agente con identificación ' + agent_id + '</div>');
+            return;
+        }
+        $.each(filtered_sales, function(index, sale) 
+        {
+            let columns = "";
+            for(let col of sale.columns)
+            {
+                columns += '<td>' + col + '</td>';
+            }
+            $('#results_table tbody').append('<tr>' + columns + '</tr>');
+
+        });
+    });
+});
