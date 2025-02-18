55d186b07eeb7218cc9564a72791109e18125295
Jose F Rivas C <josefelixrc7@gmail.com>
2025-02-18 14:55:12 -0400

    feat: Add new sales states

--- js/sales_states.js
+++ js/sales_states.js
@@ -2,7 +2,13 @@
 function GetSales()
 {
     return [
-        {agent_id: "26552160", password: "1234", dn: "1234567890", status: "ALTA", fecha_encuesta: "2025-02-01", fecha_activacion: "2025-02-02", fecha_alta: "2025-02-02", fecha_baja: "", dias_exportacion: 0}
-        ,{agent_id: "26552160", password: "1234", dn: "1234567890", status: "BAJA/EXPORTADA", fecha_encuesta: "2025-02-01", fecha_activacion: "2025-02-02", fecha_alta: "2025-02-02", fecha_baja: "2025-02-10", dias_exportacion: 8}
+        {agent_id: "7404540", password: "oJofeIs573", dn: "6567818710", status: "", fecha_encuesta: "17/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
+        {agent_id: "14326770", password: "G3BeNc5m0k", dn: "3313877775", status: "", fecha_encuesta: "17/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
+        {agent_id: "20041332", password: "NQdLDw9iyy", dn: "5524328357", status: "", fecha_encuesta: "17/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
+        {agent_id: "12504680", password: "U4SFAjmsrF", dn: "4491091098", status: "", fecha_encuesta: "17/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
+        {agent_id: "16959638", password: "rpqAdVgQpy", dn: "7821471954", status: "", fecha_encuesta: "17/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
+        {agent_id: "16959638", password: "rpqAdVgQpy", dn: "5621993662", status: "", fecha_encuesta: "17/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
+        {agent_id: "16959638", password: "rpqAdVgQpy", dn: "5621995328", status: "", fecha_encuesta: "17/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
+        {agent_id: "24601136", password: "Bkw0wwD31G", dn: "3319038968", status: "", fecha_encuesta: "17/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
     ];
 }
