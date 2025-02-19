25174351af1ebc5e0add5444e8abab135dc04bd8
Jose F Rivas C <josefelixrc7@gmail.com>
2025-02-19 17:35:03 -0400

    feat: Add new sales

--- js/sales_states.js
+++ js/sales_states.js
@@ -10,5 +10,9 @@ function GetSales()
         {agent_id: "16959638", password: "rpqAdVgQpy", dn: "5621993662", status: "", fecha_encuesta: "17/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
         {agent_id: "16959638", password: "rpqAdVgQpy", dn: "5621995328", status: "", fecha_encuesta: "17/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
         {agent_id: "24601136", password: "Bkw0wwD31G", dn: "3319038968", status: "", fecha_encuesta: "17/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
+        {agent_id: "10872105", password: "RdoICVnC5a", dn: "5515783128", status: "", fecha_encuesta: "18/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
+        {agent_id: "7623804", password: "T3pZC7n14x", dn: "9811813298", status: "", fecha_encuesta: "18/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
+        {agent_id: "6290157", password: "ap9RDJS1Je", dn: "3331572972", status: "", fecha_encuesta: "18/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
+        {agent_id: "14598801", password: "kuxuMpJtmG", dn: "3325098572", status: "", fecha_encuesta: "18/02/2025", fecha_activacion: "", fecha_alta: "", fecha_baja: "", dias_exportacion: ""},
     ];
 }
